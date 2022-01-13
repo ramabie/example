0d22a1ad47cbc544450b6c6a488fb70961327faa
ramabie <rama.biee@gmail.com>
2022-01-13 23:18:05 +0700

    appserviceprovider.php set local or production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,7 +25,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        if(App::environment(['local', 'staging']))
+        if(App::environment(['local', 'production']))
         {
             URL::forceScheme('https');
         }
