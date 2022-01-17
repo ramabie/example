359d907053ec9250815e3b59b36eb5fc6160dc0a
ramabie <rama.biee@gmail.com>
2022-01-17 11:36:38 +0700

    channge to null app_url configuration livewire

--- config/livewire.php
+++ config/livewire.php
@@ -68,7 +68,7 @@ return [
     |
     */
 
-    'app_url' => 'https://sejutapengenal.tubankab.go.id',
+    'app_url' => null,
 
     /*
     |--------------------------------------------------------------------------
