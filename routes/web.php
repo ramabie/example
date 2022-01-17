d7a5065beacedcc3826bad22c3699e295f00842b
ramabie <rama.biee@gmail.com>
2022-01-17 11:50:05 +0700

    modify welcome file to upload-livewire

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('upload-livewire');
 });
 
 Route::get('/manual-upload', [ManualUploadController::class, 'index'])->name('manual-upload.index');
